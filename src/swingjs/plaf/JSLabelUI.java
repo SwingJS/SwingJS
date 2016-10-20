46b04200491245134f1acc0e954c2ac20e68f887
hansonr <hansonr>
2016-10-20 12:26:36 +0000

    fix for label vertical offset off; unfixed problem is that some labels have outer div height and some do  not.  Don't know why.

--- src/swingjs/plaf/JSLabelUI.java
+++ src/swingjs/plaf/JSLabelUI.java
@@ -19,7 +19,7 @@ public class JSLabelUI extends JSLightweightUI {
 	protected DOMNode updateDOMNode() {
 		if (domNode == null)	
 			textNode = domNode = newDOMObject("label", id);
-		vCenter(domNode, 10);
+		//vCenter(domNode, 10);
 		DOMNode.setStyles(domNode, "position", "absolute", "width", c.getWidth() + "px",  "height", c.getHeight() + "px", "text-align", textAlign);
 		return setCssFont(DOMNode.setAttr(domNode, "innerHTML",((JLabel) c).getText()), c.getFont());
 	
