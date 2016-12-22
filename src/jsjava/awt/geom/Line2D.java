e0f66e1e92d6f38847bcc5e7477fa681b2c7e4b8
hansonr <hansonr>
2016-12-22 15:56:33 +0000

    nevermind.

--- src/jsjava/awt/geom/Line2D.java
+++ src/jsjava/awt/geom/Line2D.java
@@ -276,10 +276,7 @@ public abstract class Line2D implements Shape, Cloneable {
          * @since 1.2
          */
         public Double(double x1, double y1, double x2, double y2) {
-          this.x1 = x1;
-          this.y1 = y1;
-          this.x2 = x2;
-          this.y2 = y2;
+            setLine(x1, y1, x2, y2);
         }
 
         /**
