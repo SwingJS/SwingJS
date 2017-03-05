17d43038d051539abb97141d175395632b32db4b
hansonr <hansonr>
2017-03-05 17:47:55 +0000

    preliminary java.io.PrintWriter support

--- src/test/Test_Print.java
+++ src/test/Test_Print.java
@@ -17,6 +17,10 @@ public class Test_Print extends JApplet {
 		pw.println("testing 1");
 		pw.println("testing 2");
 		pw.println("testing 3");
+		pw.format("%4$2s %3$2s %2$2s %1$2s\n", "a", "b", "c", "d"); // -> " d  c  b  a"
+    pw.format("e = %+10.4f\n", Math.E);	   // -> "e =    +2.7183"
+    pw.format("$%(,.2f\n", -6217.581);     // -> "$(6,217.58)"
+		
 		pw.close();
 		JTextArea ja = new JTextArea(30,60);
 		ja.setText(sw.toString());
