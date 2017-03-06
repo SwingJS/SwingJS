6cb6b727885123e5e1901e5f70ad3b1dfd872c98
hansonr <hansonr>
2017-03-06 04:22:00 +0000

    Test_Print demo of PrintWriter and PrintStream in JavaScript

--- src/test/Test_Print.java
+++ src/test/Test_Print.java
@@ -3,6 +3,7 @@ package test;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -30,19 +31,17 @@ public class Test_Print extends JApplet {
 		ja.setText(sw.toString());
 		add(ja);
 
-		FileOutputStream fos;
+		// also with a PrintStream
+		
 		try {
 			File file = new File("test.txt");
-			fos = new FileOutputStream(file);
 			System.out.println(file.getAbsolutePath());
-			pw = new PrintWriter(fos);
-			pw.println(file.getAbsolutePath());
-			pw.format("%4$2s %3$2s %2$2s %1$2s\n", "a", "b", "c", "d"); // ->
-																																	// " d  c  b  a"
-			pw.format("e = %+10.4f\n", Math.E); // -> "e =    +2.7183"
-			pw.format("$%(,.2f\n", -6217.581); // -> "$(6,217.58)"
-			pw.close();
-			fos.close();
+			PrintStream ps = new PrintStream(new FileOutputStream(file));
+			ps.println(file.getAbsolutePath());
+			ps.format("%4$2s %3$2s %2$2s %1$2s\n", "a", "b", "c", "d"); // -> " d  c  b  a"
+			ps.format("e = %+10.4f\n", Math.E); // -> "e =    +2.7183"
+			ps.format("$%(,.2f\n", -6217.581); // -> "$(6,217.58)"
+			ps.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
